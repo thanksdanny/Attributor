996a131ed79d9516b1ce59d836ccfd9c40ab3cfb
thanksdanny <hj-07@163.com>
2016-03-13 14:03:52 +0800

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Danny Ho on 3/13/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
